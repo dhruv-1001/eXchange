d50abb52e61dd8070b2125ede78b374279c5a856
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2021-06-05 20:18:16 +0530

    Added Activities and splash screen functionality

---
+++ app/src/main/java/com/exchange/activity/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.exchange.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
+import com.exchange.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        val w: Window = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(applicationContext, UserLoginActivity::class.java))
+            },
+            1000
+        )
+
+    }
+}
--- app/src/main/java/com/exchange/activity/UserLoginActivity.kt
+++ app/src/main/java/com/exchange/activity/UserLoginActivity.kt
@@ -1,11 +1,12 @@
-package com.exchange
+package com.exchange.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.exchange.R
 
-class MainActivity : AppCompatActivity() {
+class UserLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_user_login)
     }
 }
