b49a47cce6b95c29647310b306b98ac68ed6e657
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2021-06-06 08:18:58 +0530

    Improved Login UI code

--- app/src/main/java/com/exchange/activity/UserLoginActivity.kt
+++ app/src/main/java/com/exchange/activity/UserLoginActivity.kt
@@ -1,20 +1,18 @@
 package com.exchange.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.view.WindowManager
+import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.exchange.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -44,7 +42,7 @@ class UserLoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
 
-        val google = findViewById<ConstraintLayout>(R.id.google)
+        val google = findViewById<LinearLayout>(R.id.googleLogin)
         auth = Firebase.auth
 
         // Configure Google Sign In
