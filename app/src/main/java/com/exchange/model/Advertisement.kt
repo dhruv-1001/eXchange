38855900a5d368204d3c5b4a3dc3cfc2a037e676
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2021-06-18 11:15:28 +0530

    created ui for sell fragment

---
+++ app/src/main/java/com/exchange/model/Advertisement.kt
@@ -0,0 +1,11 @@
+package com.exchange.model
+
+data class Advertisement(
+    var productName: String = "",
+    var productType: String = "",
+    var productDescription: String = "",
+    var userUId: String = "",
+    var productLocation: String = "",
+    var productPrice: String = "",
+    var uploadDate: String = ""
+)
